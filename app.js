007ccc79f82873c7b340fd78db04b5573c540756
suhasphamax <119290069+suhasphamax@users.noreply.github.com>
2023-07-01 17:09:11 +0530

    master

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use('/auth',authentication);
 app.use('/billing',billing);
 
 
-const PORT = process.env.PORT || 19000;
+const PORT = process.env.PORT || 8000;
+This is in master
 
 app.listen(PORT, async() =>{
     console.log('server running on http://localhost:5000');
