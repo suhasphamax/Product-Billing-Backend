3f7c31cf5a7e712bd0528af8fbf154af3e99d654
suhasphamax <119290069+suhasphamax@users.noreply.github.com>
2023-07-01 14:11:17 +0530

    port 9

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/auth',authentication);
 app.use('/billing',billing);
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, async() =>{
     console.log('server running on http://localhost:5000');
